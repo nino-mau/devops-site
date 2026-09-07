b7c52122458abaee8cf9be4d1ca8fce277b3ca06
Nino Mauuary <nino.mauuary@gmail.com>
2026-09-07 11:42:38 +0200

    📦 build: add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), tailwindcss()],
 	base: "/devops-site/",
 });
