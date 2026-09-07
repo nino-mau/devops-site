7d004cf0317b9304a899b090e192453cd62a9894
Nino Mauuary <nino.mauuary@gmail.com>
2026-09-07 11:49:24 +0200

    ✨ feat: change color of homepage text

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="flex flex-col justify-center items-center h-screen w-full">
-    <p class="text-7xl text-center">Devops Site</p>
+    <p class="text-7xl text-center text-red-500">Devops Site</p>
     <p class="text-2xl mt-5 text-center">Change 1</p>
   </div>
 </template>
