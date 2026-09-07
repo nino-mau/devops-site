86b439bde14506c48cbb1643b46f3b82624ec064
Nino Mauuary <nino.mauuary@gmail.com>
2026-09-07 12:16:09 +0200

    ⚙️ ci(github): add check h1 existence workflow

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="flex flex-col justify-center items-center h-screen w-full">
-    <p class="text-7xl text-center text-red-500">Devops Site</p>
+    <h1 class="text-7xl text-center text-red-500">Devops Site</h1>
     <p class="text-2xl mt-5 text-center">Change 1</p>
   </div>
 </template>
